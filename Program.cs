42ff87cbdd8396f8fde623d3b68aa72364caec66
tiankong1234 <44319864+tiankong1234@users.noreply.github.com>
2020-08-29 10:56:22 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using   classes;
+namespace cscs
+{
+    class Program
+    {
+ 
+static void Main(string[] args)
+{
+
+var account = new BankAccount("<name>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initialbalance.");
+Console.WriteLine(account.Balance);
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+Console.WriteLine(account.GetAccountHistory());
+
+
+
+}
+    }
+}
